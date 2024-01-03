b2b4ca9f9b5bc5135363c0d3e1963f80aa69bf4e
Bill Mårtensson <bill@magictechnology.se>
2024-01-03 09:11:09 +0100

    Initial Commit

---
+++ pia12iosklocka/pia12iosklockaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pia12iosklockaApp.swift
+//  pia12iosklocka
+//
+//  Created by BillU on 2024-01-03.
+//
+
+import SwiftUI
+
+@main
+struct pia12iosklockaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
